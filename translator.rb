649447bbd6fb115ca7831b0dc5b834b39ad88156
Thomas Perkins <baalisback@yahoo.com>
2015-10-29 13:02:08 -0500

    Update translator.rb
    Ability to translate into binary and base64

--- translator.rb
+++ translator.rb
@@ -1,12 +1,54 @@
 require 'yaml'
+require 'base64'
+
+def list
+	puts """
+	As of now Translation Center is able to translate from: 
+	
+	>Russian
+	>Spanish
+	>Arabic
+	>Pashto
+	>French
+	
+	It is also able to translate to: 
+	
+	>Base64
+	>Binary
+	
+	Please choose from the list above by typing in the name of the language you would like to translate to: 
+	"""
+	input = gets.chomp!
+	case
+	when /base64/i
+		base64
+	when /binary/i
+		binary
+	when /spanish/i
+		translate
+	when /russian/i
+		translate
+	when /arabic/i
+		translate
+	when /pashto/i
+		translate
+	when /french/i
+		translate
+	else
+		puts "Invalid input, redirecting..."
+		return
+	end
+end
 
 def welcome
-	puts "Welcome to the Translation Center! To begin press 'B', to quit press 'Q'"
+	puts "Welcome to the Translation Center! To begin press 'B', to exit the program press 'Q'. To see a list of languages available to translate type 'list:'"
 	input = language = gets.chomp!
 	if language =~ /b/i
 		translate
 	elsif language =~ /q/i
 		exit
+	elsif language =~ /list/i
+		list
 	else
 		puts "Invalid input redirecting..."
 		welcome
@@ -28,4 +70,17 @@ def translate
 		translate
 	end
 end
-welcome
+
+def binary
+	puts "Enter your text:"
+	text = gets.chomp
+	puts "#{text} in binary is: #{text.unpack('b*')}"
+end
+
+def base64
+	puts "Enter the text you want translated to Base64:"
+	input = gets.chomp!.to_s
+	new_string = "#{input}"
+	puts "'#{input}' in Base64 is: #{Base64.encode64(new_string)}"	
+end
+base64
