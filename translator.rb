4fe4b2ac5349289d85659f7c78d57c3633a64293
Thomas Perkins <baalisback@yahoo.com>
2015-10-30 13:19:42 -0500

    Update translator.rb
    Added MD5 encryption capability along with a return if user wants to keep encrypting

--- translator.rb
+++ translator.rb
@@ -1,5 +1,5 @@
 =begin
-Editor: Ekultek
+Editor: 13aal
 
 Translation Center
 
@@ -8,9 +8,12 @@ Date: 10/30/15
 
 require 'yaml'
 require 'base64'
+require 'digest'
 
-def list
+def welcome_list
 	puts """
+	Welcome to Translation Center! Below you will find a list of available(so far) features, more feautures are being added as we speak! Have fun and enjoy!
+
 	As of now Translation Center is able to translate from: 
 	
 	1: Russian
@@ -23,56 +26,42 @@ def list
 	
 	6: Base64
 	7: Binary
+	8: MD5
 	
 	Please choose from the list above by typing in the number in front of the language: 
 	"""
 	input = gets.chomp!
-	case
-	when input == "1"
+	case input.to_i
+	when 1
 		translate
-	when input == "2"
+	when 2
 		translate
-	when input == "3"
+	when 3
 		translate
-	when input == "4"
+	when 4
 		translate
-	when input == "5"
+	when 5
 		translate
-	when input == "6"
+	when 6
 		base64
-	when input =="7"
+	when 7
 		binary
+	when 8
+		md5
 	else
 		puts "Invalid input, redirecting..."
 		return
 	end
 end
 
-def welcome
-	puts "Welcome to the Translation Center! To begin press 'B', to exit the program press 'Q'. To see a list of languages available to translate type 'list:'"
-	input = language = gets.chomp!
-	if language =~ /b/i
-		translate
-	elsif language =~ /q/i
-		exit
-	elsif language =~ /list/i
-		list
-	else
-		puts "Invalid input redirecting..."
-		welcome
-	end
-end
-
 def translate
-	translations = YAML.load_file 'words.yml'
-	puts "Enter what language you want to translate from:"
-	input = language = gets.chomp
+	translations = YAML.load_file 'words.yml' 
 	puts "Enter word or phrase to be translated to English, press 'Q' to quit:"
 	input = gets.chomp
 	if translations[input]
-		puts "The translation of '#{input}', from #{language} to English is: '#{translations[input]}'"
+		puts "The translation of '#{input}' to English is: '#{translations[input]}'"
 	elsif input =~ /q/i
-		return
+		list
 	else
 		puts "Invalid word or phrase redirecting..."
 		translate
@@ -83,6 +72,7 @@ def binary
 	puts "Enter your text you wanted translated to binary:"
 	text = gets.chomp
 	puts "#{text} in binary is: #{text.unpack('b*')}"
+	binary if restart
 end
 
 def base64
@@ -90,5 +80,29 @@ def base64
 	input = gets.chomp!.to_s
 	new_string = "#{input}"
 	puts "'#{input}' in Base64 is: #{Base64.encode64(new_string)}"	
+	base64 if restart
 end
-welcome
+
+def md5
+	puts "Enter text you want translated to MD5:"
+	input = gets.chomp!.to_s
+	input_string = Digest::MD5.new
+	input_string.update(input)
+	puts "'#{input}' translated to MD5: '#{input_string}'"
+	md5 if restart
+end
+
+def restart
+	puts "Would you like to [1]translate another, go back to the [2]main menu, or [3]quit?"
+	input = gets.chomp!
+	 if input == "1"
+	 	return true
+	elsif input == "2"
+		puts "Redirecting to Menu..."
+		welcome_list
+	else
+		puts "Exiting system..."
+		exit
+	end
+end
+welcome_list
